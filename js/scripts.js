72e78000c46fa7f3cf7255d9d87685aae67ed269
Cody Fritz <clanalia55@gmail.com>
2020-06-12 12:54:19 -0700

    try a new approach to coding the function, it is not working well

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,48 @@
-$(document).ready(function() {
-  $('form#inputForm').submit(function(event) {
-    event.preventDefault();
-    let inputArray = []
 
-    inputArray.push($('input#userInput').val())
-    console.log(inputArray)
+
+    $(document).ready(function() {
+      $('form#inputForm').submit(function(event) {
+        event.preventDefault();
+
+        let finalArray = [];
+        let userNumberInput = 0;
     
-    let outputArray = mrRoboger(inputArray)
-    console.log('This is the Output', + outputArray)
-
-    $('#result').text(outputArray);
-  })
-})
-
-
-
-
-
+        userNumberInput = $('input#userInput').val();
+        console.log(userNumberInput)
+        finalArray = mrRoboger(userNumberInput);
+    
+        $('#result').text(finalArray);
+      })
+    })
     //////////////////////////////////
     /// User Interface Logic Above ///
     //////////////////////////////////
-    
+
     /////////////////////////////////
     ///   Business Logic Below.   ///
     /////////////////////////////////
-    
     function mrRoboger(number) {
       let functionArray = []
+      
       for(let index = 0; index <= number; index++) {
-        console.log(index);
-        functionArray.push(index)
+        if(index.toString().includes('3')) {
+          functionArray[index] = 'Will you be my neighbor?'
+          
+        } else if (index.toString().includes('2')) {
+           functionArray[index] = "Boop!"
+
+          } else if (index.toString().includes('1')) {
+            functionArray[index] = "Beep!"
+            
+          } else {
+            console.log(index + ' this is the end of the function');
+            functionArray[index].push(index)
+          }
+          
+        }
+        console.log(functionArray)
+        return functionArray
       }
-    
-      console.log(functionArray)
-    return functionArray
-    }
-
-
+      
 
 
