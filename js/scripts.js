6f1c30d8868d0869f2c0e0fcaef8c4a59c625e6a
Cody Fritz <clanalia55@gmail.com>
2020-06-12 13:46:24 -0700

    save progress, added css elements to improve visual appeal

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
-$(document).ready(function() {
-  $('form#inputForm').submit(function(event) {
-    event.preventDefault();
-    let finalArray = [];
-    let userNumberInput = 0;
+$(document).ready(function() { // Start your engines
+  $('form#inputForm').submit(function(event) { // Click the submit on the form!
+    event.preventDefault(); // This keeps it from breaking? 
 
-    userNumberInput = $('input#userInput').val();
+    let finalArray = []; // Set the array to empty!
+    let userNumberInput = 0; // Yep, that's a number
 
-    finalArray = mrRoboger(userNumberInput);
+    userNumberInput = $('input#userInput').val(); // Setting the number = to userInput from the form
 
-    $('#result').text(finalArray);
+    finalArray = mrRoboger(userNumberInput); // This runs the number through the function and returns it as finalArray
+
+    $('#result').text(finalArray); // This displays finalArray in Mr Robogers language
   })
 })
   //////////////////////////////////
@@ -20,24 +21,22 @@ $(document).ready(function() {
 /////////////////////////////////
 
 
-function mrRoboger(number) {
-  let returnArray = [];
+function mrRoboger(number) { // Function name and its parameter
+  let returnArray = []; // Sets the array to empty, ready for use!
 
-  for (let index = 0; index <= number; index++) {
-    if (index.toString().includes("3")) {
-      returnArray.push("Will you be my neighbor?");
+  for (let index = 0; index <= number; index++) { // Loops are EVIL but it works
+    if (index.toString().includes("3")) { // If the index were to be converted to a string, would it contain a 3?
+      returnArray.push("Will you be my neighbor?"); // If the above is true, pushes the string "Will you be my neighbor?" in place of the index
 
-    } else if (index.toString().includes("2")) {
-      returnArray.push("Boop!");
+    } else if (index.toString().includes("2")) { // If this index was a string, does it have a 2 in it?
+      returnArray.push("Boop!"); // If the above is true, push "Boop!" in its place.
 
-    } else if (index.toString().includes("1")) {
-      returnArray.push("Beep!");
+    } else if (index.toString().includes("1")) { // If this contains a 1 in string form then->
+      returnArray.push("Beep!"); // Replace it with "Beep!"
 
     } else {
-      // console.log(index + " this is the end of the function");
-      returnArray.push(index);
+      returnArray.push(index); // If it doesn't meet any of the above, BACK you go!
     }
   }
-  console.log(returnArray);
-  return returnArray
+  return returnArray // Send home for use!
 }
