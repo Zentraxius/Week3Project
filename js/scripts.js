39e5000d293f4f8d6543ebbb95fd08c1615d75e8
Cody Fritz <clanalia55@gmail.com>
2020-06-12 09:09:44 -0700

    lay out some of the framework/notes needed to proceed

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('form#userInput').submit(function(event) {
+    event.preventDefault();
+  })
+})
+// Convert Number into String
+// Use check index of in a LOOP 
+// output -> ID = #result
+// input = ID -> #userInput
